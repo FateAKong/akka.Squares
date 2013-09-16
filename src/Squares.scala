89b87aece773b4705ca7617c4e08ba4f20478a92
FateAKong <azurelfsun@gmail.com>
2013-09-16 17:23:45 -0400

    Merge branch 'develop' into master
    Finish performance optimization and code simplification

--- src/Squares.scala
+++ src/Squares.scala
@@ -5,28 +5,23 @@
  * Time: 8:23 PM
  */
 
-import akka.actor._
-import akka.routing.RoundRobinRouter
-import scala.collection.mutable.TreeSet
-import scala.math._
+import akka.actor.{Actor, ActorSystem, Props, ActorRef}
+import scala.math.sqrt
 
 object Squares extends App {
 
   if (args.length == 2) {
-    findSeqs(nWorkers = 2, nElements = args(1).toInt, nMsgs = args(0).toInt)
+    findSeqs(nWorkers = 4, nElements = args(1).toInt, nMsgs = args(0).toInt)
   }
 
   def findSeqs(nWorkers: Int, nElements: Int, nMsgs: Int) {
     // Create an Akka system
     val system = ActorSystem("SquaresSystem")
 
-    // create the result listener, which will print the result and shutdown the system
-    val listener = system.actorOf(Props[Listener], name = "listener")
-
     // create the master
-    val master = system.actorOf(Props(new Master(nWorkers, nElements, nMsgs, listener)), name = "master")
+    val master = system.actorOf(Props(new Master(nWorkers, nElements, nMsgs)), name = "master")
 
-    // startOfSeq the calculation
+    // start the calculation
     master ! Assign
 
   }
@@ -37,64 +32,47 @@ sealed trait SquaresMsg
 case object Assign extends SquaresMsg
 
 // only one Calculate message is permitted during entire execution
-case class Work(start: Int, nrOfElements: Int) extends SquaresMsg
+case class Work(startOfLoad: Int, workLoad: Int, nElements: Int) extends SquaresMsg
 
-case class Result(startOfSeq: Int) extends SquaresMsg
-
-case class ResultsSet(seqsSet: TreeSet[Int]) extends SquaresMsg
+case object Done extends SquaresMsg
 
 class Worker extends Actor {
 
-  def verifySeq(startOfSeq: Int, nElements: Int): Int = {
-    var sumOfSquares = 0.0
-    for (i ← startOfSeq until (startOfSeq + nElements)) {
-      // calculate square using Long to avoid potential Int overflow problems
-      // e.g. 1000000 24 should output 30 sequences instead of 36 using Int
-      sumOfSquares += i.toLong * i.toLong
-    }
-    val sqrtOfSum = sqrt(sumOfSquares)
-    if (sqrtOfSum == ceil(sqrtOfSum))
-      startOfSeq
-    else
-      0 - startOfSeq
-  }
-
   def receive = {
-    case Work(start, nElements) ⇒
-      sender ! Result(verifySeq(start, nElements)) // perform the work
+    case Work(startOfLoad, workLoad, nElements) ⇒
+      for (startOfSeq <- startOfLoad to startOfLoad + workLoad - 1) {
+        // Use sum of squares equation to calculate sum of a sequence's squares
+        // Here use Long to avoid potential Int overflow problems
+        // e.g. 1000000 24 should output 30 sequences instead of 36 using Int
+        val start: Long = startOfSeq - 1
+        val end: Long = start + nElements
+        val sumOfSquares = (end * (end + 1) * (2 * end + 1) - start * (start + 1) * (2 * start + 1)) / 6
+        val sqrtOfSum = sqrt(sumOfSquares)
+        if (sqrtOfSum == sqrtOfSum.toLong)
+          println(startOfSeq)
+        sender ! Done
+      }
   }
 }
 
-class Master(nWorkers: Int, nElements: Int, nMsgs: Int, listener: ActorRef)
+class Master(nWorkers: Int, nElements: Int, nMsgs: Int)
   extends Actor {
 
-  var seqsSet = new TreeSet[Int]
-  var nResults: Int = _
+  var workLoad = nMsgs / nWorkers
+  var nDone: Int = _
 
-  val workerRouter = context.actorOf(
-    Props[Worker].withRouter(RoundRobinRouter(nWorkers)), name = "workerRouter")
+  val workers = new Array[ActorRef](nWorkers)
+  for (i ← 0 to nWorkers - 1)
+    workers(i) = context.actorOf(Props[Worker])
 
   def receive = {
     case Assign ⇒
-      for (i ← 1 to nMsgs) workerRouter ! Work(i, nElements)
-    case Result(startOfSeq) ⇒
-      if (startOfSeq > 0)
-        seqsSet += startOfSeq
-      nResults += 1
-      if (nResults == nMsgs) {
-        // Send the result to the listener
-        listener ! ResultsSet(seqsSet)
-        // Stops this actor and all its supervised children
-        context.stop(self)
+      for (i ← 0 to nWorkers - 1) workers(i) ! Work(i * workLoad + 1, workLoad, nElements)
+      workers(0) ! Work(nWorkers * workLoad + 1, nMsgs - nWorkers * workLoad, nElements)
+    case Done ⇒
+      nDone += 1
+      if (nDone == nMsgs) {
+        context.system.shutdown()
       }
-  }
-
-}
-
-class Listener extends Actor {
-  def receive = {
-    case ResultsSet(seqsSet) ⇒
-      seqsSet.foreach(println)
-      context.system.shutdown()
   }
 }
